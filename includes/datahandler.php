0e3e8bde04dbaf5da80f49ed74059f618b3f7ef4
Abby Chen <chen1049429066@gmail.com>
2020-03-29 23:27:30 -0600

    cleaned up data validation and added posting

--- includes/datahandler.php
+++ includes/datahandler.php
@@ -1,3 +1,77 @@
 <?php 
 
+class DataHandler {
+    //TODO: Prevent SQL injections and make hex code generator
+
+    private $fname;
+
+    private $hasLname = false;
+    private $lname;
+
+    private $email;
+
+    private $hasNumber = false;
+    private $number;
+
+    private $hexId;
+
+    public function __construct($fname, $lname, $email, $number) {
+        if(!isset($fname) || !isset($email)) {
+            throw new InvalidArgumentException("First name and email not set");
+        }
+
+        $this->fname = $fname;
+        $this->email = $email;
+
+        if(isset($lname)) {
+            $this->hasLname = true;
+            $this->lname = $lname;
+        }
+
+        if(isset($number)) {
+            $this->hasNumber = true;
+            $this->number = $number;
+        }
+
+        //Generate hex code from email
+        $this->hexId = bin2hex($this->email);
+    }
+
+    function postData() {
+        //Post data to: https://webhook.site/d419ed99-5060-49c1-bff0-c699a4af5da4
+        $url = "https://webhook.site/d419ed99-5060-49c1-bff0-c699a4af5da4";
+        $curl = curl_init($url);
+
+        $data = [
+            "first_name" => $this->fname,
+            "email" => $this->email,
+            "hex_id" => $this->hexId
+        ];
+
+        #echo $this->fname;
+        #echo $this->email;
+        #echo $this->hexId;
+
+        if($this->hasLname) {
+            $data["last_name"] = $this->lname;
+            #echo $this->lname;
+        }
+
+        if($this->hasNumber) {
+            $data["number"] = $this->number;
+            #echo $this->number;
+        }
+
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            "APIKEY: 111111111111111111111111111",
+            "Content-Type: application/json",
+         ]);
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
+    }
+}
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
+
+include "includes/dataHandler.php";
+
 // define variables and set to empty values
-$nameErr = $emailErr = "";
+$fnameErr = $lnameErr = $emailErr = $numErr = "";
 $fname = $lname = $email = $number = "";
+$successMessage = "";
 
 function cleanInput($data) {
   $data = trim($data);
@@ -11,16 +15,29 @@ function cleanInput($data) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $validFname = $validEmail = false;
+  $validFname = $validEmail = $validNumber = $validLname = false;
   if (empty($_POST["first_name_input"])) {
-    $nameErr = "*Name required";
+    $fnameErr = "*Name required";
   } else {
     $fname = cleanInput($_POST["first_name_input"]);
     // check if name only contains letters and whitespace
     if (!preg_match("/^[a-zA-Z ]*$/",$fname)) {
-      $nameErr = "No numbers/special characters";
+      $fnameErr = "No numbers/special characters";
     } else {
       $validFname = true;
+    }
+  }
+
+  if (empty($_POST["last_name_input"])) {
+    $lname = null;
+    $validLname = true;
+  } else {
+    $lname = cleanInput($_POST["last_name_input"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$lname)) {
+      $lnameErr = "No numbers/special characters";
+    } else {
+      $validLname = true;
     }
   }
 
@@ -36,8 +53,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
   }
 
+  if (empty($_POST["number_input"])) {
+    $number = null;
+    $validNumber = true;
+  } else {
+    $number = cleanInput($_POST["number_input"]);
+    //check that number has 7 digits and no letters
+    if(preg_match("/^[^a-zA-Z]$/", $number)) {
+      $numErr = "*Invalid phone number" . $number;
+    } else {
+      $number = preg_replace("/[^0-9]/","", $number);
+      if(strlen($number) != 10) {
+        $numErr = "*Expecting a 10-digit phone number";
+      } else {
+        $validNumber = true;
+      }
+    }
+  }
+
   if($validEmail && $validFname) {
-    //postData($fname, $email);
+    $dataHandler = new DataHandler($fname, $lname, $email, $number);
+    echo $dataHandler->postData();
+    $successMessage = "Thank you for your submission!";
   }
 }
 ?>
@@ -62,18 +99,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <form id=inputs method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
             <label for="first_name_input">First name*:</label>
             <input type="text" name="first_name_input" placeholder="John" size="19"/>
-            <span class="error"><?php echo $nameErr;?></span><br><br>
+            <span class="error"><?php echo $fnameErr;?></span><br><br>
             <label for="last_name_input">Last Name:</label>
-            <input type="text" name="last_name_input" placeholder="Doe"/><br><br>
+            <input type="text" name="last_name_input" placeholder="Doe"/>
+            <span class="error"><?php echo $lnameErr;?></span><br><br>
             <label for="email">Email*:</label>
             <input type="text" name="email_input" placeholder="example@example.com" size="24"/>
             <span class="error"><?php echo $emailErr;?></span><br><br>
             <label for="number_input">Phone Number:</label>
-            <input type="text" name="number_input" placeholder="###-###-####" size="16"/><br><br><br>
+            <input type="text" name="number_input" placeholder="###-###-####" size="16"/>
+            <span class="error"><?php echo $numErr;?></span><br><br><br>
             <div id="button"><input type="submit" class="btn btn-info" value="Submit Form"></div>
         </form>
+        <?echo $successMessage?>
     <div/>
-
+<!--
+    <div id=results>
+      <h1 class=title>Thank you for Submitting! Here's what we have:</h1>
+      <div id=outputs>
+          <div class=inp>
+              First name: <?php //echo $fname; ?>
+          </div>
+          <div class=inp>
+              Last name: <?php //echo $lname; ?>
+          </div>
+          <div class=inp>
+              Email Address: <?php //echo $email; ?>
+          </div>
+          <div class=inp>
+              Phone Number: <?php //echo $number; ?>
+          </div>
+          <div class=inp>
+              Unique Hex Code: <?php //echo $hexCode; ?>
+          </div>
+      </div>
+    </div>
+-->
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
